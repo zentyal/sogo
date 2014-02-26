3a671643aefefa0b37c75409affa6b190b1bf4b1
Wolfgang Sourdeau <Wolfgang@Contre.COM>
2014-02-25 23:18:28 -0500

    added test ensuring that using json templates still enables json posts

--- Tests/Integration/test-json-forms.py
+++ Tests/Integration/test-json-forms.py
@@ -10,42 +10,47 @@ import webdavlib
 
 
 class JSONFormsTests(unittest.TestCase):
-  def setUp(self):
-    self.client = webdavlib.WebDAVClient(hostname, port)
+    def testJsonContactCreationViaSo(self):
+        self._doJsonContactCreation("so")
 
-  def testJsonContactCreation(self):
-    # json connect
-    form_data = {"userName": username,
-                 "password": password}
-    data = json.dumps(form_data)
-    post = webdavlib.HTTPPOST("/SOGo/connect", data,
-                              content_type = "application/json")
-    self.client.execute(post)
-    self.assertEquals(post.response["status"], 200)
+    def testJsonContactCreationViaJson(self):
+        self._doJsonContactCreation("json")
+    def _doJsonContactCreation(self, prefix):
+        client = webdavlib.WebDAVClient(hostname, port)
 
-    # retrieve auth cookie for further ops
-    cookie = post.response["headers"]["set-cookie"]
-    parts = cookie.split(";")
-    login_value = parts[0].strip()
-    event_uid = "json-contact"
-    base_url = "/SOGo/so/%s/Contacts/personal/%s.vcf" % (username, event_uid)
+        # json connect
+        form_data = {"userName": username,
+                     "password": password}
+        data = json.dumps(form_data)
 
-    # delete old version of future card if exists
-    post = webdavlib.HTTPPOST("%s/delete" % base_url, "")
-    post.cookie = login_value
-    self.client.execute(post)
+        post = webdavlib.HTTPPOST("/SOGo/%s/connect" % prefix, data,
+                                  content_type = "application/json")
+        client.execute(post)
+        self.assertEquals(post.response["status"], 200)
 
-    # create card
-    card_data = {"givenname": "Json",
-                 "sn": "Card",
-                 "displayname": "Json Card",
-                 "mail": "nouvelle@carte.com"}
-    data = json.dumps(card_data)
-    post = webdavlib.HTTPPOST("%s/saveAsContact" % base_url,
-                              data, content_type = "application/json")
-    post.cookie = login_value
-    self.client.execute(post)
-    self.assertEquals(post.response["status"], 200)
+        # retrieve auth cookie for further ops
+        cookie = post.response["headers"]["set-cookie"]
+        parts = cookie.split(";")
+        login_value = parts[0].strip()
+        card_uid = "json-contact-%s" % prefix
+        base_url = "/SOGo/%s/%s/Contacts/personal/%s.vcf" % (prefix, username, card_uid)
+
+        # delete old version of future card if exists
+        post = webdavlib.HTTPPOST("%s/delete" % base_url, "")
+        post.cookie = login_value
+        client.execute(post)
+
+        # create card
+        card_data = {"givenname": "Json",
+                     "sn": "Card",
+                     "displayname": "Json Card",
+                     "mail": "nouvelle@carte.com"}
+        data = json.dumps(card_data)
+        post = webdavlib.HTTPPOST("%s/saveAsContact" % base_url,
+                                  data, content_type = "application/json")
+        post.cookie = login_value
+        client.execute(post)
+        self.assertEquals(post.response["status"], 200)
 
 if __name__ == "__main__":
     sogotests.runTests()
